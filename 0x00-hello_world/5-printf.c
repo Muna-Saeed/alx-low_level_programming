bda738f42e345fd420cc0913c2285a3cc96494d0
Muna Said <munasaeed6367@gmail.com>
2023-06-15 14:48:49 -0700

    a C program that prints exactly with proper grammar, but the outcome is a piece of art, followed by a new line added

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - function simply prints to the console using the puts function then
+ * main - function simply prints to the console using the printf function then
  * Return: returns 0 to indicate a successful execution
  */
 int main(void)
