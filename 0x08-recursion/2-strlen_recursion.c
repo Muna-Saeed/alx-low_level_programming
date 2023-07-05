f4c8f684afa0318e85c34f39f0f217e10b824ed8
Muna Said <munasaeed6367@gmail.com>
2023-07-05 14:33:44 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,12 +7,13 @@
  */
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
+	int longit = 0;
+
+	if (*s)
 	{
-		return 0;
+		longit++;
+		longit += _strlen_recursion(s + 1);
 	}
-	else
-	{
-		return 1 + _strlen_recursion(s + 1);
-	}
+
+	return (longit);
 }
