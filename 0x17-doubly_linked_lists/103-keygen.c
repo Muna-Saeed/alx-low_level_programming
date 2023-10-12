152353c5258339b71e661f4ee387e4461e5aa15e
Muna-Saeed <munasaeed6367@gmail.com>
2023-10-12 15:57:30 -0700

    crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -2,31 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-void generate_key(char* username, char* key) {
-    int i;
-    int length = strlen(username);
+/**
+ * generate_key - Generates a key based on the username.
+ * @username: The username.
+ * @key: The generated key.
+ */
+void generate_key(char *username, char *key)
+{
+	int i;
+	int length = strlen(username);
 
-    for (i = 0; i < length; i++) {
-        key[i] = username[i] ^ 0x17;
-    }
+	for (i = 0; i < length; i++)
+	{
+		key[i] = username[i] ^ 0x17;
+	}
 
-    key[length] = '\0';
+	key[length] = '\0';
 }
 
-int main(int argc, char* argv[]) {
-    char* username;
-    char key[256];
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ * Return: 0 on success, 1 on error.
+ */
+int main(int argc, char *argv[])
+{
+	char *username;
+	char key[256];
 
-    if (argc != 2) {
-        printf("Usage: %s <username>\n", argv[0]);
-        return 1;
-    }
+	if (argc != 2)
+	{
+		printf("Usage: %s <username>\n", argv[0]);
+		return (1);
+	}
 
-    username = argv[1];
+	username = argv[1];
 
-    generate_key(username, key);
+	generate_key(username, key);
 
-    printf("%s\n", key);
+	printf("%s\n", key);
 
-    return 0;
+	return (0);
 }
